1cca5df4e73077913dc54bf1b40246faa2871895
Marcos Magueta <maguetamarcos@gmail.com>
2024-10-14 22:23:28 -0300

    heap structure procedures

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -5,7 +5,58 @@ module AST = struct
     | Functor of { name: tag; elements: t list; arity: int }
 end
 
+module Heap = struct
+  type 'a t = 'a Seq.t
+  let push (heap: 'a t) (data: 'a) =
+    Seq.cons data heap
+  let pop (heap: 'a t) =
+    if Seq.is_empty heap
+    then heap
+    else Seq.drop 1 heap
+  let top (heap: 'a t) =
+    Option.map (function (value, _) -> value) @@ Seq.uncons heap
+  let put (elem: 'a) (index: int) (heap: 'a t): 'a t =
+    failwith "Not implemented"
+end
+
 module Evaluation = struct
+  module AbstractMachine = struct
+    module Cell = struct
+      type t =
+        | Structure of int
+        | Reference of int
+        | Functor of string * int
+    end
+
+    open Cell
+    
+    type computer =
+      { heap: Cell.t Heap.t;
+        registers: Cell.t list;
+        h_register: int }
+    
+    (* Creates a new structure on the heap *)
+    let put_structure (index_of_register: int) (functor_label, functor_arity) {heap; registers; h_register} =
+      let stru = Structure (h_register + 1) in
+      let func = Functor (functor_label, functor_arity) in
+      let heap = Heap.put func (h_register + 1) @@ Heap.put stru h_register heap in
+      let registers = List.mapi (fun i elem -> if i = index_of_register then stru else elem) registers in
+      let h_register = h_register + 2
+      in { heap; registers; h_register }
+
+    let set_variable (index_of_register: int) {heap; registers; h_register} =
+      let reference = Reference h_register in
+      let heap = Heap.put reference h_register heap in
+      let registers = List.mapi (fun i elem -> if i = index_of_register then reference else elem) registers in
+      let h_register = h_register + 1
+      in { heap; registers; h_register }
+
+    let set_value (index_of_register: int) {heap; registers; h_register} =
+      let value_of_register = List.nth registers index_of_register in
+      let heap = Heap.put value_of_register h_register heap in
+      let h_register = h_register + 1
+      in { heap; registers; h_register }
+  end
 
   module Map = Map.Make(String)
   (* L0 defines the program as a single term *)
