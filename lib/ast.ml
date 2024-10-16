ceb43406c2da3035c0b7076fef984adadaa66bba
EduardoLR10 <dudulr10@gmail.com>
2024-10-15 22:41:04 -0300

    Format

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,7 +1,7 @@
 let say_hi () = print_endline "Hello, Karuta!"
 
 type tag = string
-type t =
-  | Variable of { name: tag }
-  | Functor of { name: tag; elements: t list; arity: int }
 
+type t =
+  | Variable of { name : tag }
+  | Functor of { name : tag; elements : t list; arity : int }
