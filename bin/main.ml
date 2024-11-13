4632a0793250771d5403aa7760b5edc7c113d5a1
Marcos Magueta <maguetamarcos@gmail.com>
2024-11-12 22:24:28 -0300

    fix parser

--- bin/main.ml
+++ bin/main.ml
@@ -4,12 +4,11 @@ end
 
 let _ =
   begin
-    let fc = open_in "examples/test.krt" in
     let open Option in
-    let+ content = try Some(input_line fc)
-                   with End_of_file -> None in
-    let () = close_in fc in
-    match Lib.Parse.parse content with
+    let+ content = In_channel.with_open_text
+                     "examples/test.krt"
+                     (fun fc -> try Some(In_channel.input_all fc) with End_of_file -> None)
+    in match Lib.Parse.parse content with
     | [] -> print_endline "File could not be parsed."; None
     | asts -> print_endline @@ Lib.Ast.show_ts asts; None
   end
