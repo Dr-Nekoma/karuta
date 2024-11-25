a7970f9991611fae39f9340749bebdbf7a7283ff
Marinho <marinho.pinotti@gmail.com>
2024-11-25 00:04:43 -0300

    Call register allocation by its proper name

--- lib/compiler.ml
+++ lib/compiler.ml
@@ -14,13 +14,13 @@ let rec compile : Ast.t list * t * Cell.t Store.t -> t * Cell.t Store.t =
   | [], compiler, store -> (compiler, store)
   | [ d ], compiler, store -> (
       match d with
-      | Query f -> compile_functor f compiler store
+      | Query f -> register_alloc_functor f compiler store
       | Variable _ | Functor _ -> failwith "unreachable"
       | Declaration { head; body } ->
-          compile_declaration head body compiler store)
+          register_alloc_declaration head body compiler store)
   | _, _, _ -> failwith "TODO"
 
-and compile_loop : t -> Cell.t Store.t -> t * Cell.t Store.t =
+and register_alloc_loop : t -> Cell.t Store.t -> t * Cell.t Store.t =
  fun ({ terms; _ } as compiler) store ->
   match FT.front terms with
   | None -> (compiler, store)
@@ -28,27 +28,29 @@ and compile_loop : t -> Cell.t Store.t -> t * Cell.t Store.t =
       let new_compiler = { compiler with terms = rest } in
       match d with
       | Declaration _ -> failwith "unreachable"
-      | Variable v -> compile_variable v new_compiler store
-      | Query f | Functor f -> compile_functor f new_compiler store)
+      | Variable v -> register_alloc_variable v new_compiler store
+      | Query f | Functor f -> register_alloc_functor f new_compiler store)
 
-and compile_declaration :
+and register_alloc_declaration :
     Ast.func -> Ast.func list -> t -> Cell.t Store.t -> t * Cell.t Store.t =
  fun head body ({ terms; _ } as compiler) store ->
   match body with
   | [] ->
-      compile_loop
+      register_alloc_loop
         { compiler with terms = FT.cons terms (Ast.Functor head) }
         store
   | _ -> failwith "TODO"
 
-and compile_functor : Ast.func -> t -> Cell.t Store.t -> t * Cell.t Store.t =
+and register_alloc_functor :
+    Ast.func -> t -> Cell.t Store.t -> t * Cell.t Store.t =
   let open RegisterMap in
   fun ({ elements; _ } as func) { terms; registers } store ->
     let new_registers = add (Ast.Functor func) (cardinal registers) registers in
     let new_terms = FT.append terms (FT.of_list elements) in
-    compile_loop { registers = new_registers; terms = new_terms } store
+    register_alloc_loop { registers = new_registers; terms = new_terms } store
 
-and compile_variable : Ast.var -> t -> Cell.t Store.t -> t * Cell.t Store.t =
+and register_alloc_variable :
+    Ast.var -> t -> Cell.t Store.t -> t * Cell.t Store.t =
   let open RegisterMap in
   fun var ({ registers; _ } as compiler) store ->
     match find_opt (Ast.Variable var) registers with
@@ -56,5 +58,5 @@ and compile_variable : Ast.var -> t -> Cell.t Store.t -> t * Cell.t Store.t =
         let new_registers =
           add (Ast.Variable var) (cardinal registers) registers
         in
-        compile_loop { compiler with registers = new_registers } store
-    | Some _ -> compile_loop compiler store
+        register_alloc_loop { compiler with registers = new_registers } store
+    | Some _ -> register_alloc_loop compiler store
