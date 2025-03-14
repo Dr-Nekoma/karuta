b0b802fb5ec17fe5f4c8d1d61d528e9aa3fdf19a
EduardoLR10 <dudulr10@gmail.com>
2025-03-14 20:53:41 -0300

    Refactor machine module

--- lib/Machine.ml
+++ lib/Machine.ml
@@ -68,14 +68,8 @@ let show_store (store : Cell.t Store.t) (how_many : int option) : string =
     ""
     (limit_list @@ Store.to_list store)
 
-(* type maguetas_type = (int*Cell.t) list *)
-(* [@@deriving show] *)
-
 let show_x_registers (registers : Cell.t IntMap.t) : string =
   let open IntMap in
-  (* let new_map: maguetas_type ref = ref [] in *)
-  (* IntMap.iter (fun (k: int) (v: Cell.t) -> new_map := (k, v)::(!new_map)) registers; *)
-  (* show_maguetas_type (!new_map); *)
   fold
     (fun key value acc ->
       acc ^ "\n" ^ string_of_int key ^ " = " ^ Cell.show value)
@@ -86,7 +80,6 @@ let initialize () : t =
   {
     store = Store.initialize Store.empty Store.mem_size Cell.Empty;
     x_registers = IntMap.empty;
-    (* IM.empty ~eq:( = ); *)
     p_register = 0;
     cp_register = 0;
     e_register = Store.stack_start;
