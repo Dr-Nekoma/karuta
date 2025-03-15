839e0cf4264dd63e33b5c4a46ec5cba4b9ca6ba8
EduardoLR10 <dudulr10@gmail.com>
2025-03-14 22:21:27 -0300

    Refactor code generator module

--- lib/CodeGenerator.ml
+++ lib/CodeGenerator.ml
@@ -24,13 +24,18 @@ let initialize (begin_addr : int) : t =
     scope_registers = S.empty;
   }
 
+let put_allocator (allocator : RegisterAllocator.t)
+    ((generator, store) : t * Cell.t Store.t) :
+    t * RegisterAllocator.t * Cell.t Store.t =
+  (generator, allocator, store)
+
 let cell_register : RegisterAllocator.register -> Cell.register = function
   | Temporary value -> Cell.X value
   | Permanent value -> Cell.Y value
 
-and add_instruction
-    ((({ p_register; _ } as generator), store) : t * Cell.t Store.t)
-    (instruction : Cell.instruction) : t * Cell.t Store.t =
+and add_instruction (instruction : Cell.instruction)
+    ((({ p_register; _ } as generator), store) : t * Cell.t Store.t) :
+    t * Cell.t Store.t =
   let store = Store.code_put (Cell.Instruction instruction) p_register store in
   ({ generator with p_register = p_register + 1 }, store)
 
@@ -56,7 +61,7 @@ module Fact = struct
     let open RegisterAllocator.RegisterMap in
     let register = cell_register @@ find elem registers in
     let instruction = Cell.UnifyVariable register in
-    let generator, store = add_instruction (generator, store) instruction in
+    let generator, store = add_instruction instruction (generator, store) in
     match elem with
     | Variable { namev } ->
         ({ generator with variables = S.add namev variables }, allocator, store)
@@ -73,11 +78,12 @@ module Fact = struct
     match elem with
     | Variable _ ->
         let instruction = Cell.UnifyVariable register in
-        let generator, store = add_instruction (generator, store) instruction in
-        (generator, allocator, store)
+        (generator, store)
+        |> add_instruction instruction
+        |> put_allocator allocator
     | Functor { namef; arity; elements } ->
         let instruction = Cell.GetStructure ((namef, arity), register) in
-        let generator, store = add_instruction (generator, store) instruction in
+        let generator, store = add_instruction instruction (generator, store) in
         List.fold_left emit_nested_argument
           (generator, allocator, store)
           elements
@@ -89,12 +95,11 @@ module Fact = struct
       t * RegisterAllocator.t * Cell.t Store.t =
     match FT.front terms with
     | None -> (generator, allocator, store)
-    | Some (rest, d) ->
-        let generator = { generator with terms = rest } in
-        let result =
-          emit_queue_nested_argument (generator, allocator, store) d
-        in
-        emit_queue_arguments result
+    | Some (rest, elem) ->
+        elem
+        |> emit_queue_nested_argument
+             ({ generator with terms = rest }, allocator, store)
+        |> emit_queue_arguments
 
   and emit_argument
       ((({ variables; _ } as generator), ({ registers; _ } as allocator), store) :
@@ -110,13 +115,12 @@ module Fact = struct
           | None -> (S.add namev variables, Cell.UnifyVariable arg_register)
           | Some _ -> (variables, Cell.GetValue (register, arg_register))
         in
-        let generator, store =
-          add_instruction ({ generator with variables }, store) instruction
-        in
-        (generator, allocator, store)
+        ({ generator with variables }, store)
+        |> add_instruction instruction
+        |> put_allocator allocator
     | Functor { namef; arity; elements } ->
         let instruction = Cell.GetStructure ((namef, arity), arg_register) in
-        let generator, store = add_instruction (generator, store) instruction in
+        let generator, store = add_instruction instruction (generator, store) in
         List.fold_left emit_nested_argument
           (generator, allocator, store)
           elements
@@ -153,14 +157,14 @@ module Argument = struct
           | None -> (S.add namev variables, variable register)
           | Some _ -> (variables, value register)
         in
-        let generator, store =
-          add_instruction ({ generator with variables }, store) instruction
-        in
-        (generator, allocator, store)
+        ({ generator with variables }, store)
+        |> add_instruction instruction
+        |> put_allocator allocator
     | _ ->
         let instruction = catchall register in
-        let generator, store = add_instruction (generator, store) instruction in
-        (generator, allocator, store)
+        (generator, store)
+        |> add_instruction instruction
+        |> put_allocator allocator
 
   and emit_query
       ((generator, ({ registers; _ } as allocator), store) :
@@ -177,7 +181,7 @@ module Argument = struct
     match elem with
     | Functor { namef; elements; arity } ->
         let instruction = Cell.PutStructure ((namef, arity), register) in
-        let generator, store = add_instruction (generator, store) instruction in
+        let generator, store = add_instruction instruction (generator, store) in
         List.fold_left emit_query (generator, allocator, store) elements
     | _ -> emit_toplevel_query_argument (generator, allocator, store) elem
 
@@ -201,15 +205,15 @@ let rec allocate_head ({ elements; _ } : Ast.func)
     let open RegisterAllocator.RegisterMap in
     let raw_register = find element registers in
     let register = cell_register raw_register in
-    let instruction = Cell.GetVariable (register, Cell.X counter) in
     let scope_registers = S.add raw_register scope_registers in
+    let instruction = Cell.GetVariable (register, Cell.X counter) in
     let generator, store =
-      add_instruction ({ generator with scope_registers }, store) instruction
+      add_instruction instruction ({ generator with scope_registers }, store)
     in
     ((generator, allocator, store), counter + 1)
   in
   let instruction = Cell.Allocate y_register in
-  let generator, store = add_instruction (generator, store) instruction in
+  let generator, store = add_instruction instruction (generator, store) in
   List.fold_left head_folder ((generator, allocator, store), 0) elements |> fst
 
 and allocate_body (elements : Ast.func list) (generator, allocator, store) :
@@ -239,9 +243,8 @@ and allocate_body (elements : Ast.func list) (generator, allocator, store) :
                 (scope_registers, Cell.PutValue (left_register, Cell.X counter))
           in
           let generator, store =
-            add_instruction
+            add_instruction instruction
               ({ generator with scope_registers }, store)
-              instruction
           in
           ((generator, allocator, store), counter + 1)
       | Functor func as f ->
@@ -249,29 +252,28 @@ and allocate_body (elements : Ast.func list) (generator, allocator, store) :
           let (generator, allocator, store), _ =
             (generate (generator, allocator, store) (Ast.Query func), counter)
           in
-          let raw_register = find f registers in
-          let left_register = cell_register raw_register in
+          let left_register = cell_register @@ find f registers in
           let instruction = Cell.PutValue (left_register, Cell.X counter) in
           let generator, store =
-            add_instruction (generator, store) instruction
+            add_instruction instruction (generator, store)
           in
           ((generator, allocator, store), counter + 1)
       | _ -> failwith "unreachable allocate_body"
     in
-    let instruction = Cell.Call (namef, arity) in
     let (generator, allocator, store), _ =
       List.fold_left allocate_argument
         ((generator, allocator, store), 0)
         elements
     in
-    let generator, store = add_instruction (generator, store) instruction in
-    (generator, allocator, store)
+    let instruction = Cell.Call (namef, arity) in
+    (generator, store) |> add_instruction instruction |> put_allocator allocator
   in
   let generator, allocator, store =
     List.fold_left allocate_clause (generator, allocator, store) elements
   in
-  let generator, store = add_instruction (generator, store) Cell.Deallocate in
-  (generator, allocator, store)
+  (generator, store)
+  |> add_instruction Cell.Deallocate
+  |> put_allocator allocator
 
 and generate
     ((generator, ({ registers; _ } as allocator), store) :
@@ -286,33 +288,33 @@ and generate
           elements
       in
       let instruction = Cell.Call (namef, arity) in
-      let generator, store = add_instruction (generator, store) instruction in
-      let generator, store = add_instruction (generator, store) Cell.Halt in
-      ({ generator with variables = S.empty }, allocator, store)
+      (generator, store)
+      |> add_instruction instruction
+      |> add_instruction Cell.Halt
+      |> (fun (generator, store) ->
+           ({ generator with variables = S.empty }, store))
+      |> put_allocator allocator
   | Functor { namef; elements; arity } ->
       let register = cell_register @@ find value registers in
       let instruction = Cell.GetStructure ((namef, arity), register) in
-      let generator, store = add_instruction (generator, store) instruction in
-      let generator, allocator, store =
+      let generator, store = add_instruction instruction (generator, store) in
+      let gas =
         List.fold_left Argument.emit_functor
           (generator, allocator, store)
           elements
       in
-      let generator, allocator, store =
-        List.fold_left generate (generator, allocator, store) elements
-      in
+      let generator, allocator, store = List.fold_left generate gas elements in
       ({ generator with variables = S.empty }, allocator, store)
   | Variable _ -> (generator, allocator, store)
   | Declaration { head = { elements; _ }; body = [] } ->
-      let generator, allocator, store =
+      let gas =
         Seq.fold_lefti Fact.emit_argument
           (generator, allocator, store)
           (List.to_seq elements)
       in
-      let generator, allocator, store =
-        Fact.emit_queue_arguments (generator, allocator, store)
-      in
-      let generator, store = add_instruction (generator, store) Cell.Proceed in
-      (generator, allocator, store)
+      let generator, allocator, store = Fact.emit_queue_arguments gas in
+      (generator, store)
+      |> add_instruction Cell.Proceed
+      |> put_allocator allocator
   | Declaration { head; body } ->
       (generator, allocator, store) |> allocate_head head |> allocate_body body
