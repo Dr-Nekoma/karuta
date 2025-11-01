b3d591d9663857744f0de87d754f0bff3924321f
Marinho <marinho.pinotti@gmail.com>
2025-11-01 13:58:19 -0300

    Fix register allocation
    Right now, it's possible to clobber a temporary register before we should
    when a predicate with a lower arity calls one with a higher arity and no
    permanent registers are involved. This fix takes care of that by starting
    the register allocation on the lowest temporary register that is unused by
    the predicate's arguments and all of its callees.

--- lib/RegisterAllocator.ml
+++ lib/RegisterAllocator.ml
@@ -101,7 +101,11 @@ and not_debug ({ namef; arity; _ } : Ast.func) : bool =
   not (namef = "debug" && arity = 0)
 
 and allocate_declaration : Ast.decl -> t =
- fun { head = { elements; _ } as head; body } ->
+ fun { head = { elements; arity; _ } as head; body } ->
+  let first_non_argument_register =
+    List.fold_left max arity
+    @@ List.map (fun ({ arity; _ } : Ast.func) -> arity) body
+  in
   let first_clause, other_clauses =
     match List.filter not_debug body with
     | [] -> ([], [])
@@ -133,7 +137,7 @@ and allocate_declaration : Ast.decl -> t =
     {
       initial_allocator with
       permanent_variables;
-      x_register = List.length elements;
+      x_register = first_non_argument_register;
     }
     terms
 
