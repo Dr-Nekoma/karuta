3bf34bda468554e789afe101c24750c6190b2430
EduardoLR10 <dudulr10@gmail.com>
2025-11-02 02:09:32 -0300

    Improve error handling when missing file for compilation

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 let _ =
   (* TODO: Fix this CLI on Issue 11 *)
-  let file = Sys.argv.(1) in
-  match Lib.Executor.run file with
-  | None -> failwith @@ "Could not execute file: " ^ file
-  | Some computer -> print_endline @@ Lib.Crawler.query_string computer
+  if Array.length Sys.argv = 2 then
+    let file = Sys.argv.(1) in
+    match Lib.Executor.run file with
+    | None -> failwith @@ "Could not execute file: " ^ file
+    | Some computer -> print_endline @@ Lib.Crawler.query_string computer
+  else failwith "Missing file to compile"
